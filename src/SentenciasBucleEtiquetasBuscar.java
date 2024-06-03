bb794050c488b1de079f3955649b63f38b10be91
DylanVallejo <dylan1811vallejo@gmail.com>
2024-06-02 22:38:49 -0500

    mejorando la ejecucion del ciclo

--- src/SentenciasBucleEtiquetasBuscar.java
+++ src/SentenciasBucleEtiquetasBuscar.java
@@ -3,21 +3,23 @@ public class SentenciasBucleEtiquetasBuscar {
         String frase = "tres tristes tigres tragan trigo en un trigal";
         String palabra  = "trigo";
 
-
-        int max = frase.length();
         int maxPalabra = palabra.length();
+        int max = frase.length() - maxPalabra + 1;
 
         int cantidad = 0;
         char letra = 'g';
         buscar:
-        for(int i =0; i < max; i++){
+        for(int i =0; i < max;){
             int k = i;
             for (int j =0; j< maxPalabra; j++){
                 if(frase.charAt(k++) != palabra.charAt(j)){
+                    i++;
+                    System.out.println("entra" + frase.charAt(k++));
                     continue buscar;
                 }
             }
             cantidad++;
+            i = i + maxPalabra;
         }
         System.out.println("Encontrado  = " + cantidad +" vez la palabra: " + palabra + "  en la frase");
     }
