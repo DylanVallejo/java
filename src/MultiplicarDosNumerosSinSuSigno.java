a18e4c358dd844b753902a77f086858a738856c5
DylanVallejo <dylan1811vallejo@gmail.com>
2024-06-03 11:43:56 -0500

    multiplicar dos numeros teneinedo en cuenta sus signos sin el signo de multiplicacion *

---
+++ src/MultiplicarDosNumerosSinSuSigno.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MultiplicarDosNumerosSinSuSigno {
+
+    public static void main(String[] args) {
+
+        int total = 0;
+        int valorUno = 0;
+        int valorDos = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el primer valor = ");
+        valorUno = scanner.nextInt();
+        System.out.println("Ingrese el segundo valor = ");
+        valorDos = scanner.nextInt();
+        int valorPositivos = Math.abs(valorDos);
+
+        for(int i = 0; i < valorPositivos; i++){
+            total += valorUno;
+        }
+
+        if ((valorUno < 0 && valorPositivos > 0) || (valorUno > 0 && valorPositivos < 0)) {
+            total = -total;
+        }
+        System.out.println("total = " + total);
+
+    }
+}
