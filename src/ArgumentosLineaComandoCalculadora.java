195265a9f3ee97666caa4f6a1432f52de6a8f9b0
DylanVallejo <dylan1811vallejo@gmail.com>
2024-11-14 23:54:26 -0500

    uso de la linea de comandos para recibir argumentos

---
+++ src/ArgumentosLineaComandoCalculadora.java
@@ -0,0 +1,39 @@
+public class ArgumentosLineaComandoCalculadora {
+    public static void main(String[] args) {
+
+
+        if (args.length != 3){
+            System.out.println("Por favor ingresar una operacion (suma, resta, multiplicacion , o division ) y dos enteros.");
+            System.exit(-1);
+        }
+        String operacion = args[0];
+
+        int a = Integer.parseInt(args[1]);
+        int b = Integer.parseInt(args[2]);
+        double resultado = 0.00;
+
+        switch (operacion){
+            case "suma":
+                resultado = a + b;
+                break;
+            case "resta":
+                resultado = a - b;
+                break;
+            case "multiplicacion":
+                resultado = a * b;
+                break;
+            case "dividir":
+                if (b == 0 ){
+                    System.exit(-1);
+                    System.out.println("no se puede dividir por cero");
+                }
+                resultado = (double) a / b;
+                break;
+            default:
+                resultado = a+b;
+        }
+
+        System.out.println("El resultado de la operacion  "+operacion + "es : " + resultado);
+
+    }
+}
