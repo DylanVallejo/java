21a6f63ce5964e109c3e311d7a20a0fa1fd54f5d
DylanVallejo <dylan1811vallejo@gmail.com>
2024-11-17 22:27:06 -0500

    saving the day

--- src/ArgumentosLineaComandoCalculadora.java
+++ src/ArgumentosLineaComandoCalculadora.java
@@ -31,7 +31,7 @@ public class ArgumentosLineaComandoCalculadora {
                 break;
             default:
                 resultado = a+b;
-        }//jess lawizz
+        }//jess lawizz papa chees
 
 
         System.out.println("El resultado de la operacion  "+operacion + "es : " + resultado);
