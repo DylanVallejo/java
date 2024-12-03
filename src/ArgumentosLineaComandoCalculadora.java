5611f0b9646affe09534896dca5b54ed60475465
DylanVallejo <dylan1811vallejo@gmail.com>
2024-12-02 19:39:02 -0500

    saving the day

--- src/ArgumentosLineaComandoCalculadora.java
+++ src/ArgumentosLineaComandoCalculadora.java
@@ -31,7 +31,7 @@ public class ArgumentosLineaComandoCalculadora {
                 break;
             default:
                 resultado = a+b;
-        }  //idk idkqql
+        }
 
         System.out.println("El resultado de la operacion  "+operacion + "es : " + resultado);
 
