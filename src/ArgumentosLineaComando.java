ab9d7d510c83deb63bbce2ad62ded17d6db44f4b
DylanVallejo <dylan1811vallejo@gmail.com>
2024-12-23 23:17:21 -0500

    saving the day

--- src/ArgumentosLineaComando.java
+++ src/ArgumentosLineaComando.java
@@ -2,7 +2,7 @@ public class ArgumentosLineaComando {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("debe inkgresar argumentos");
+            System.out.println("debe ingresar argumentos");
             System.exit(-1);
         }
         for (int i = 0; i < args.length; i++) {
