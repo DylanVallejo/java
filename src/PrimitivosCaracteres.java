9b3ecf2f1e298a0e3329976452f5d9cca76e15a9
DylanVallejo <dylan1811vallejo@gmail.com>
2024-12-28 23:32:29 -0500

    saving the day

--- src/PrimitivosCaracteres.java
+++ src/PrimitivosCaracteres.java
@@ -10,18 +10,18 @@ public class PrimitivosCaracteres {
 
         char simbolo = '@';
         System.out.println("simbolo = " + simbolo);
-        System.out.println("simbolo = caracter: " + (simbolo==caracter));
+        System.out.println("simbolo = caracter: " + (simbolo == caracter));
 
         char espacio = '\u0020';
         char retroceso = '\b';
         char tabulador = '\t';
         char nuevaLinea = '\n';
         char retrocesosCarro = '\r';
-        System.out.println("tipo char corresponde en byte  a:" + espacio + Character.BYTES);
+        System.out.println("tipo char corresponde en byte  a : " + espacio + Character.BYTES);
         System.out.println("tipo char corresponde en bit a : "+ Character.SIZE);
         System.out.println("valor maximo de un double: "+ Character.MAX_VALUE);
         System.out.println("valor minimo de un double: "+ Character.MIN_VALUE);
-        System.out.println("/////// /////////////");
+        System.out.println("////////////////////");
 
 
     }
