3edc747a7d8f6e7df4dee1d733ba2df05f0ab0c0
DylanVallejo <dylan1811vallejo@gmail.com>
2025-01-05 23:37:37 -0500

    aplicando el algoritmo de ordenamiento burbuja

--- src/arreglos/EjemploArreglosForOrdenamientoBurbuja.java
+++ src/arreglos/EjemploArreglosForOrdenamientoBurbuja.java
@@ -22,16 +22,17 @@ public class EjemploArreglosForOrdenamientoBurbuja {
 
     public static void main(String[] args) {
 
-        String[] productos = {"Kingston pendrive 64gb", "Samsung galaxy","Disco duro SSD Samsung externo"};
+        String[] productos = {"Kingston pendrive 64gb", "Samsung galaxy","Disco duro SSD Samsung externo", "Asus notebook", "Bitcoin"};
         int total = productos.length;
 
         int contador = 0;
-        for(int i =0; i<total; i++ ){
-            for( int j = 0; j <total; j++ ){
-                if (productos[i].compareTo(productos[j]) < 0 ){
-                    String auxiliarIntermedia = productos[i];
-                    productos[i] = productos[j];
-                    productos[j] = auxiliarIntermedia;
+        for(int i =0; i< total - 1; i++ ){
+            for( int j = 0; j < total -1 -i ; j++ ){
+//                compara el elemento actual contra el siguiente el ordenamiento es case sensitive
+                if (productos[j+1].compareTo(productos[j]) < 0 ){
+                    String auxiliarIntermedia = productos[j];
+                    productos[j] = productos[j+1];
+                    productos[j+1] = auxiliarIntermedia;
                 }
                 contador++;
             }
