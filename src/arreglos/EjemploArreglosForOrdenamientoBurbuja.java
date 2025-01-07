623490f9cfa02b0c2ceab9140cf4ed3e0e84af30
DylanVallejo <dylan1811vallejo@gmail.com>
2025-01-06 23:28:59 -0500

    aplicando el uso de calses genericas e implmentanddo un metodo generico para el metodo burbuja

--- src/arreglos/EjemploArreglosForOrdenamientoBurbuja.java
+++ src/arreglos/EjemploArreglosForOrdenamientoBurbuja.java
@@ -2,6 +2,7 @@ package arreglos;
 
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class EjemploArreglosForOrdenamientoBurbuja {
 
@@ -20,33 +21,50 @@ public class EjemploArreglosForOrdenamientoBurbuja {
 
     }
 
+    public static void sortBurbuja(Object[] arreglo) {
+        int total = arreglo.length;
+
+        int contador = 0;
+        for(int i =0; i < total - 1; i++ ){
+            for( int j = 0; j < total -1 -i ; j++ ){
+//                compara el elemento actual contra el siguiente el ordenamiento es case sensitive
+                if (( (Comparable) arreglo[j+1]).compareTo(arreglo[j]) < 0 ){
+                    Object auxiliarIntermedia = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = auxiliarIntermedia;
+                }
+                contador++;
+            }
+        }
+        System.out.println("contador = " + contador);
+
+    }
+
     public static void main(String[] args) {
 
         String[] productos = {"Kingston pendrive 64gb", "Samsung galaxy","Disco duro SSD Samsung externo", "Asus notebook", "Bitcoin"};
         int total = productos.length;
 
-        int contador = 0;
-        for(int i =0; i< total - 1; i++ ){
-            for( int j = 0; j < total -1 -i ; j++ ){
-//                compara el elemento actual contra el siguiente el ordenamiento es case sensitive
-                if (productos[j+1].compareTo(productos[j]) < 0 ){
-                    String auxiliarIntermedia = productos[j];
-                    productos[j] = productos[j+1];
-                    productos[j+1] = auxiliarIntermedia;
-                }
-                contador++;
-            }
-        }
-//        Arrays.sort(productos);
-//        invirtiendo mediante el api de Collections
-//        Collections.reverse(Arrays.asList(productos));
-
-        arregloInverso(productos);
+        sortBurbuja(productos);
         System.out.println(" ==== usando for ==== ");
-        System.out.println("contador = " + contador);
+
         for (int i = 0; i < total; i++ ){
             System.out.println("para el indice " + i + " : " + productos[i]);
         }
+
+        Integer[] numeros = new Integer[4];
+        numeros[0] = 10;
+        numeros[1] = Integer.valueOf("7");
+        numeros[2] = 35;
+        numeros[3] = -1;
+
+
+        sortBurbuja(numeros);
+        for (int i = 0; i < numeros.length; i ++){
+            System.out.println("i = " + i + " : "+ numeros[i]);
+        }
+
+
 
     }
 }
