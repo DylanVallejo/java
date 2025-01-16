c0e24a1a48cd39d6ed68d23c62ae115a95f6b442
DylanVallejo <dylan1811vallejo@gmail.com>
2025-01-15 23:44:36 -0500

    save

--- src/arreglos/EjemploArreglosOrdenPrincipioFinal.java
+++ src/arreglos/EjemploArreglosOrdenPrincipioFinal.java
@@ -3,8 +3,6 @@ package arreglos;
 public class EjemploArreglosOrdenPrincipioFinal {
     public static void main(String[] args) {
         int[] numeros = new int[10];
-        for (int numero : numeros){
-            System.out.println(numero);
-        }
+
     }
 }
