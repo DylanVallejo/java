ddbe5c1f5663ada93825e5b256cd848fa7113ce5
DylanVallejo <dylan1811vallejo@gmail.com>
2025-02-21 22:52:17 -0500

    ejemplo calculo de año biciesto leap year

---
+++ src/pruebas/LeapYearChecker.java
@@ -0,0 +1,25 @@
+package pruebas;
+
+public class LeapYearChecker {
+
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int year = 2025; // Puedes cambiar este valor para probar otros años
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
